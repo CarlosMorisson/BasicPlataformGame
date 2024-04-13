75404e5ab17f37627a8f93b133898feddf1b6d98
carlos <carlos.lescanomorisson@gmail.com>
2024-04-13 11:30:57 -0400

    first commit

---
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,285 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour, IPlayerController
+{
+    [SerializeField] private SoStats _stats;
+    private GameObject player;
+    private Rigidbody2D _rb;
+    private CapsuleCollider2D _col;
+    private FrameInput _frameInput;
+    private Vector2 _frameVelocity;
+    private bool _cachedQueryStartInColliders;
+
+    #region Interface
+
+    public Vector2 FrameInput => _frameInput.Move;
+    public event Action<bool, float> GroundedChanged;
+    public event Action Jumped;
+
+    #endregion
+    private float _time;
+
+    public GameObject Player => player;
+
+    private TrailRenderer _dashTrail;
+    private GameObject _shadownPlayer;
+    private Animator _playerAnimator;
+    private void Awake()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        _shadownPlayer = GameObject.FindGameObjectWithTag("ShadowPlayer");
+        _rb = player.GetComponent<Rigidbody2D>();
+        _col = player.GetComponent<CapsuleCollider2D>();
+        _dashTrail = player.GetComponentInChildren<TrailRenderer>();
+        _playerAnimator = player.GetComponentInChildren<Animator>();
+
+        _cachedQueryStartInColliders = Physics2D.queriesStartInColliders;
+    }
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        GatherInput();
+    }
+
+    private void GatherInput()
+    {
+        _frameInput = new FrameInput
+        {
+            JumpDown = Input.GetButtonDown("Jump"),
+            JumpHeld = Input.GetButton("Jump"),
+            DashDown = Input.GetButtonDown("Dash"),
+            Move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")),
+            BackTimeDown = Input.GetKeyDown(KeyCode.E)
+        };
+
+        if (_frameInput.JumpDown)
+        {
+            _jumpToConsume = true;
+            _timeJumpWasPressed = _time;
+        }
+        if (_frameInput.BackTimeDown)
+        {
+            _backTimeToConsume = true;
+        }
+        if (_frameInput.DashDown)
+        {
+            _dashToConsume = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CheckCollisions();
+
+        HandleJump();
+        HandleDirection();
+        HandleGravity();
+        HadleDash();
+
+        ApplyMovement();
+
+        HandleBackTime();
+    }
+
+    #region Collisions
+
+    private float _frameLeftGrounded = float.MinValue;
+    private bool _grounded;
+
+    private void CheckCollisions()
+    {
+        Physics2D.queriesStartInColliders = false;
+
+        // Ground and Ceiling
+        bool groundHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, _stats.GrounderDistance, ~_stats.PlayerLayer);
+        bool ceilingHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.up, _stats.GrounderDistance, ~_stats.PlayerLayer);
+
+        // Hit a Ceiling
+        if (ceilingHit) _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
+
+        // Landed on the Ground
+        if (!_grounded && groundHit)
+        {
+            _grounded = true;
+            _coyoteUsable = true;
+            _bufferedJumpUsable = true;
+            _endedJumpEarly = false;
+            GroundedChanged?.Invoke(true, Mathf.Abs(_frameVelocity.y));
+        }
+        // Left the Ground
+        else if (_grounded && !groundHit)
+        {
+            _grounded = false;
+            _frameLeftGrounded = _time;
+            GroundedChanged?.Invoke(false, 0);
+        }
+
+        Physics2D.queriesStartInColliders = _cachedQueryStartInColliders;
+    }
+
+    #endregion
+
+    #region Jumping
+    private bool _jumpToConsume;
+    private float _jumpsToConsume;
+    private bool _bufferedJumpUsable;
+    private bool _endedJumpEarly;
+    private bool _coyoteUsable;
+    private float _timeJumpWasPressed;
+
+    private bool HasBufferedJump => _bufferedJumpUsable && _time < _timeJumpWasPressed + _stats.JumpBuffer;
+    private bool CanUseCoyote => _coyoteUsable && !_grounded && _time < _frameLeftGrounded + _stats.CoyoteTime;
+
+    private void HandleJump()
+    {
+        if (!_endedJumpEarly && !_grounded && !_frameInput.JumpHeld && _rb.velocity.y > 0) 
+            _endedJumpEarly = true;
+
+        if(_grounded || _backTimeActive)
+            _jumpsToConsume = _stats.MaxJumps;
+        
+        if (!_jumpToConsume && !HasBufferedJump) 
+            return;
+        if (CanUseCoyote || _jumpsToConsume > 0) 
+            ExecuteJump();
+
+        _jumpToConsume = false;
+    }
+
+    private void ExecuteJump()
+    {
+        _playerAnimator.CrossFade("Jump Animation", 0);
+        _jumpsToConsume -= 1;
+        _endedJumpEarly = false;
+        _timeJumpWasPressed = 0;
+        _bufferedJumpUsable = false;
+        _coyoteUsable = false;
+        _frameVelocity.y = _stats.JumpPower;
+        Jumped?.Invoke();
+    }
+    #endregion
+
+    #region Horizontal
+
+    private void HandleDirection()
+    {
+        if (_frameInput.Move.x == 0)
+        {
+            float deceleration = _grounded ? _stats.GroundDeceleration : _stats.AirDeceleration;
+            _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
+            if (_frameVelocity.x == 0 && _grounded)
+                _playerAnimator.CrossFade("Idle Animation", 0);
+        }
+        else
+        {
+            _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * _stats.MaxSpeed, _stats.Acceleration * Time.fixedDeltaTime);
+            if(_grounded)
+                _playerAnimator.CrossFade("Run Animation", 0);
+        }
+            
+
+    }
+
+    #endregion
+
+    #region Gravity
+
+    private void HandleGravity()
+    {
+        if (_grounded && _frameVelocity.y <= 0f)
+            _frameVelocity.y = _stats.GroundingForce;
+
+        else
+        {
+            float inAirGravity = _stats.FallAcceleration;
+            if (_endedJumpEarly && _frameVelocity.y > 0) 
+                inAirGravity *= _stats.JumpEndEarlyGravityModifier;
+            
+            _frameVelocity.y = Mathf.MoveTowards(_frameVelocity.y, -_stats.MaxFallSpeed, inAirGravity * Time.fixedDeltaTime);
+        }
+    }
+
+    #endregion
+
+    #region Dashing
+    private bool _dashToConsume;
+    private float _timeLastDash = float.MinValue;
+    private float _dashingTime;
+    private void HadleDash()
+    {
+        if (_dashToConsume && _time > _timeLastDash + _stats.DashCooldown)
+            StartCoroutine(ExecuteDash());
+        _dashToConsume = false;
+    }
+    private IEnumerator ExecuteDash()
+    {
+        _dashTrail.emitting = true;
+        _frameVelocity.x *= _stats.DashPower;
+        _timeLastDash = _time;
+        yield return new WaitForSeconds(_dashingTime = _stats.DashingTime);
+        _dashTrail.emitting = false;
+        _dashToConsume = false;
+    }
+    #endregion
+
+    #region BackTime
+    private float _backTimeCoolDown;
+    private float _fullBackTimeCoolDown;
+    private bool _canCountCoolDown=true;
+    private bool _backTimeToConsume;
+    private bool _backTimeActive;
+    private void HandleBackTime()
+    {
+        _fullBackTimeCoolDown = _stats.FullBackTimeCooldown;
+        if (_backTimeToConsume && _backTimeCoolDown >= _fullBackTimeCoolDown)
+            ExecuteBackTime();
+        if(_canCountCoolDown)
+        {
+            if (_backTimeCoolDown < _fullBackTimeCoolDown)
+            {
+                _backTimeCoolDown += Time.deltaTime;
+                _backTimeActive = false;
+                //coolDownBar.fillAmount = coolDownShadow / coolDownTotalShadow;
+            }
+            else
+            {
+                _canCountCoolDown = false;
+                _shadownPlayer.gameObject.SetActive(true);
+            }
+        }
+    }
+    private void ExecuteBackTime()
+    {
+        _backTimeCoolDown = 0;
+        player.transform.localPosition = _shadownPlayer.transform.localPosition;
+        ShadowPlayerController.instance._playerPositions.Clear();
+        _shadownPlayer.SetActive(false);
+        _backTimeActive = true;
+        _canCountCoolDown = true;
+        _backTimeToConsume = false;
+    }
+    #endregion
+
+    private void ApplyMovement() => _rb.velocity = _frameVelocity;
+
+}
+public interface IPlayerController
+{
+    public event Action<bool, float> GroundedChanged;
+
+    public event Action Jumped;
+    public Vector2 FrameInput { get; }
+}
+public struct FrameInput
+{
+    public bool JumpDown;
+    public bool JumpHeld;
+    public bool DashDown;
+    public Vector2 Move;
+    public bool BackTimeDown;
+}
